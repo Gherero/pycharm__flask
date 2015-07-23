b4dc508e86b1dd249a842bb22f10b8459aad93d2
gherero <katerina18>
2015-07-23 17:27:13 +0300

    test

--- forms.py
+++ forms.py
@@ -1,2 +1,10 @@
 __author__ = 'kiro'
-#from flask import
+from wtforms import Form,BooleanField,TextField,PasswordField,validators
+
+class Registform(Form):
+    name=TextField('Name',[validators.Length(min=2,max=60)])
+    lastname=TextField('Lastname',[validators.Length(min=2,max=60)])
+    internet=BooleanField('internet')
+    mail=BooleanField('mail')
+    file_server=BooleanField('file_server')
+
